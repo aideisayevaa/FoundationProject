b27ab2360983be98a7f0ab6ff51017b290da74d1
aideisayevaa <aideisayeva3@gmail.com>
2021-07-31 17:55:30 +0400

    Eolymp-den birnece misala baxildi

---
+++ Algorithms/eolymp/app.py
@@ -0,0 +1,52 @@
+#       902
+
+""" sagirdinBali=int(input())
+if sagirdinBali>=1 and sagirdinBali<=3:
+    print("Initial")
+elif sagirdinBali>=4 and sagirdinBali<=6:
+    print("Average")
+elif sagirdinBali>=7 and sagirdinBali<=9:
+    print("Sufficient")
+elif sagirdinBali>=10 and sagirdinBali<=12:
+    print("High") """
+
+
+
+#          903
+
+""" eded=int(input())
+yuzluk=eded//100
+teklik=eded%10
+if yuzluk>teklik:
+    print(yuzluk)
+elif teklik>yuzluk:
+    print(teklik)
+else:
+    print("=") """
+
+
+#        905
+
+""" a,b,c=input().split()
+
+if a==b==c:
+    print(1)
+elif a==b or a==c or b==c:
+    print(2)
+else:
+    print(3) """
+
+
+
+#        906
+
+""" eded=int(input())
+yuzluk=eded//100
+onluq=eded//10%10
+teklik=eded%10
+cavab=yuzluk*onluq*teklik
+print(cavab) """
+
+
+
+
